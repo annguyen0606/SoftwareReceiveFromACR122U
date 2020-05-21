7ca7f6f5a6c28ea700e77ef98e8f9fa348e54820
annguyen2106 <59633843+annguyen2106@users.noreply.github.com>
2020-05-21 10:53:15 +0700

    1

---
+++ DemoNhanDuLieu/DemoNhanDuLieu/Form1.cs
@@ -0,0 +1,401 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DemoNhanDuLieu
+{
+    public partial class Form1 : Form
+    {
+        int retCode;
+        int hCard;
+        int hContext;
+        int Protocol;
+        public bool connActive = false;
+        string readername = "";      // change depending on reader
+        public byte[] SendBuff = new byte[263];
+        public byte[] RecvBuff = new byte[263];
+        public int SendLen, RecvLen, nBytesRet, reqType, Aprotocol, dwProtocol, cbPciLength;
+        public Card.SCARD_READERSTATE RdrState;
+        public Card.SCARD_IO_REQUEST pioSendRequest;
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            if (connectCard())
+            {
+                string cardUID = getcardUID();
+                //textBox1.Text = Int64.Parse(cardUID, System.Globalization.NumberStyles.HexNumber).ToString();
+                textBox1.Text = cardUID; //displaying on text block
+            }
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            if (connectCard())// establish connection to the card: you've declared this from previous post
+            {
+                submitText(textBox2.Text, "5"); // 5 - is the block we are writing data on the card
+                Close();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SelectDevice();
+            establishContext();
+        }
+        public void SelectDevice()
+        {
+            List<string> availableReaders = this.ListReaders();
+            this.RdrState = new Card.SCARD_READERSTATE();
+            readername = availableReaders[1].ToString();//selecting first device
+            this.RdrState.RdrName = readername;
+        }
+
+        internal void establishContext()
+        {
+            retCode = Card.SCardEstablishContext(Card.SCARD_SCOPE_SYSTEM, 0, 0, ref hContext);
+            if (retCode != Card.SCARD_S_SUCCESS)
+            {
+                MessageBox.Show("Check your device and please restart again", "Reader not connected", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                connActive = false;
+                return;
+            }
+        }
+
+        public List<string> ListReaders()
+        {
+            int ReaderCount = 0;
+            List<string> AvailableReaderList = new List<string>();
+
+            //Make sure a context has been established before 
+            //retrieving the list of smartcard readers.
+            retCode = Card.SCardListReaders(hContext, null, null, ref ReaderCount);
+            if (retCode != Card.SCARD_S_SUCCESS)
+            {
+                MessageBox.Show(Card.GetScardErrMsg(retCode));
+                //connActive = false;
+            }
+
+            byte[] ReadersList = new byte[ReaderCount];
+
+            //Get the list of reader present again but this time add sReaderGroup, retData as 2rd & 3rd parameter respectively.
+            retCode = Card.SCardListReaders(hContext, null, ReadersList, ref ReaderCount);
+            if (retCode != Card.SCARD_S_SUCCESS)
+            {
+                MessageBox.Show(Card.GetScardErrMsg(retCode));
+            }
+
+            string rName = "";
+            int indx = 0;
+            if (ReaderCount > 0)
+            {
+                // Convert reader buffer to string
+                while (ReadersList[indx] != 0)
+                {
+
+                    while (ReadersList[indx] != 0)
+                    {
+                        rName = rName + (char)ReadersList[indx];
+                        indx = indx + 1;
+                    }
+
+                    //Add reader name to list
+                    AvailableReaderList.Add(rName);
+                    rName = "";
+                    indx = indx + 1;
+
+                }
+            }
+            return AvailableReaderList;
+        }
+
+        public bool connectCard()
+        {
+            connActive = true;
+
+            retCode = Card.SCardConnect(hContext, readername, Card.SCARD_SHARE_SHARED,
+                      Card.SCARD_PROTOCOL_T0 | Card.SCARD_PROTOCOL_T1, ref hCard, ref Protocol);
+
+            if (retCode != Card.SCARD_S_SUCCESS)
+            {
+                MessageBox.Show(Card.GetScardErrMsg(retCode), "Card not available", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                connActive = false;
+                return false;
+            }
+            return true;
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            string text = verifyCard("5"); // 5 - is the block we are reading
+            textBox3.Text = text.ToString();
+        }
+
+        private string getcardUID()//only for mifare 1k cards
+        {
+            string cardUID = "";
+            byte[] receivedUID = new byte[256];
+            Card.SCARD_IO_REQUEST request = new Card.SCARD_IO_REQUEST();
+            request.dwProtocol = Card.SCARD_PROTOCOL_T1;
+            request.cbPciLength = System.Runtime.InteropServices.Marshal.SizeOf(typeof(Card.SCARD_IO_REQUEST));
+            byte[] sendBytes = new byte[] { 0xFF, 0xCA, 0x00, 0x00, 0x00 }; //get UID command      for Mifare cards
+            int outBytes = receivedUID.Length;
+            int status = Card.SCardTransmit(hCard, ref request, ref sendBytes[0], sendBytes.Length, ref request, ref receivedUID[0], ref outBytes);
+
+            if (status != Card.SCARD_S_SUCCESS)
+            {
+                cardUID = "Error";
+            }
+            else
+            {
+                cardUID = BitConverter.ToString(receivedUID.Take(7).ToArray()).Replace("-", string.Empty).ToLower();
+            }
+            return cardUID;
+        }
+
+        public void submitText(String Text, String Block)
+        {
+
+            String tmpStr = Text;
+            int indx;
+            if (authenticateBlock(Block))
+            {
+                ClearBuffers();
+                SendBuff[0] = 0xFF;                             // CLA
+                SendBuff[1] = 0xD6;                             // INS
+                SendBuff[2] = 0x00;                             // P1
+                SendBuff[3] = (byte)int.Parse(Block);           // P2 : Starting Block No.
+                SendBuff[4] = (byte)int.Parse("16");            // P3 : Data length
+
+                for (indx = 0; indx <= (tmpStr).Length - 1; indx++)
+                {
+                    SendBuff[indx + 5] = (byte)tmpStr[indx];
+                }
+                SendLen = SendBuff[4] + 5;
+                RecvLen = 0x02;
+
+                retCode = SendAPDUandDisplay(2);
+
+                if (retCode != Card.SCARD_S_SUCCESS)
+                {
+                    MessageBox.Show("fail write");
+
+                }
+                else
+                {
+                    MessageBox.Show("write success");
+                }
+            }
+            else
+            {
+                MessageBox.Show("FailAuthentication");
+            }
+        }
+
+        // block authentication
+        private bool authenticateBlock(String block)
+        {
+            ClearBuffers();
+            SendBuff[0] = 0xFF;                         // CLA
+            SendBuff[2] = 0x00;                         // P1: same for all source types 
+            SendBuff[1] = 0x86;                         // INS: for stored key input
+            SendBuff[3] = 0x00;                         // P2 : Memory location;  P2: for stored key input
+            SendBuff[4] = 0x05;                         // P3: for stored key input
+            SendBuff[5] = 0x01;                         // Byte 1: version number
+            SendBuff[6] = 0x00;                         // Byte 2
+            SendBuff[7] = (byte)int.Parse(block);       // Byte 3: sectore no. for stored key input
+            SendBuff[8] = 0x60;                         // Byte 4 : Key A for stored key input
+            SendBuff[9] = (byte)int.Parse("1");         // Byte 5 : Session key for non-volatile memory
+
+            SendLen = 0x0A;
+            RecvLen = 0x02;
+
+            retCode = SendAPDUandDisplay(0);
+
+            if (retCode != Card.SCARD_S_SUCCESS)
+            {
+                //MessageBox.Show("FAIL Authentication!");
+                return false;
+            }
+
+            return true;
+        }
+
+        // clear memory buffers
+        private void ClearBuffers()
+        {
+            long indx;
+
+            for (indx = 0; indx <= 262; indx++)
+            {
+                RecvBuff[indx] = 0;
+                SendBuff[indx] = 0;
+            }
+        }
+
+        // send application protocol data unit : communication unit between a smart card reader and a smart card
+        private int SendAPDUandDisplay(int reqType)
+        {
+            int indx;
+            string tmpStr = "";
+
+            pioSendRequest.dwProtocol = Aprotocol;
+            pioSendRequest.cbPciLength = 8;
+
+            //Display Apdu In
+            for (indx = 0; indx <= SendLen - 1; indx++)
+            {
+                tmpStr = tmpStr + " " + string.Format("{0:X2}", SendBuff[indx]);
+            }
+
+            retCode = Card.SCardTransmit(hCard, ref pioSendRequest, ref SendBuff[0],
+                                 SendLen, ref pioSendRequest, ref RecvBuff[0], ref RecvLen);
+
+            if (retCode != Card.SCARD_S_SUCCESS)
+            {
+                return retCode;
+            }
+
+            else
+            {
+                try
+                {
+                    tmpStr = "";
+                    switch (reqType)
+                    {
+                        case 0:
+                            for (indx = (RecvLen - 2); indx <= (RecvLen - 1); indx++)
+                            {
+                                tmpStr = tmpStr + " " + string.Format("{0:X2}", RecvBuff[indx]);
+                            }
+
+                            if ((tmpStr).Trim() != "90 00")
+                            {
+                                //MessageBox.Show("Return bytes are not acceptable.");
+                                return -202;
+                            }
+
+                            break;
+
+                        case 1:
+
+                            for (indx = (RecvLen - 2); indx <= (RecvLen - 1); indx++)
+                            {
+                                tmpStr = tmpStr + string.Format("{0:X2}", RecvBuff[indx]);
+                            }
+
+                            if (tmpStr.Trim() != "90 00")
+                            {
+                                tmpStr = tmpStr + " " + string.Format("{0:X2}", RecvBuff[indx]);
+                            }
+
+                            else
+                            {
+                                tmpStr = "ATR : ";
+                                for (indx = 0; indx <= (RecvLen - 3); indx++)
+                                {
+                                    tmpStr = tmpStr + " " + string.Format("{0:X2}", RecvBuff[indx]);
+                                }
+                            }
+
+                            break;
+
+                        case 2:
+
+                            for (indx = 0; indx <= (RecvLen - 1); indx++)
+                            {
+                                tmpStr = tmpStr + " " + string.Format("{0:X2}", RecvBuff[indx]);
+                            }
+
+                            break;
+                    }
+                }
+                catch (IndexOutOfRangeException)
+                {
+                    return -200;
+                }
+            }
+            return retCode;
+        }
+
+        //disconnect card reader connection
+        public void Close()
+        {
+            if (connActive)
+            {
+                retCode = Card.SCardDisconnect(hCard, Card.SCARD_UNPOWER_CARD);
+            }
+            //retCode = Card.SCardReleaseContext(hCard);
+        }
+
+        public string verifyCard(String Block)
+        {
+            string value = "";
+            if (connectCard())
+            {
+                value = readBlock(Block);
+            }
+
+            value = value.Split(new char[] { '\0' }, 2, StringSplitOptions.None)[0].ToString();
+            return value;
+        }
+
+        public string readBlock(String Block)
+        {
+            string tmpStr = "";
+            int indx;
+
+            if (authenticateBlock(Block))
+            {
+                ClearBuffers();
+                SendBuff[0] = 0xFF; // CLA 
+                SendBuff[1] = 0xB0;// INS
+                SendBuff[2] = 0x00;// P1
+                SendBuff[3] = (byte)int.Parse(Block);// P2 : Block No.
+                SendBuff[4] = (byte)int.Parse("16");// Le
+
+                SendLen = 5;
+                RecvLen = SendBuff[4] + 2;
+
+                retCode = SendAPDUandDisplay(2);
+
+                if (retCode == -200)
+                {
+                    return "outofrangeexception";
+                }
+
+                if (retCode == -202)
+                {
+                    return "BytesNotAcceptable";
+                }
+
+                if (retCode != Card.SCARD_S_SUCCESS)
+                {
+                    return "FailRead";
+                }
+
+                // Display data in text format
+                for (indx = 0; indx <= RecvLen - 1; indx++)
+                {
+                    tmpStr = tmpStr + Convert.ToChar(RecvBuff[indx]);
+                }
+
+                return (tmpStr);
+            }
+            else
+            {
+                return "FailAuthentication";
+            }
+        }
+    }
+}
